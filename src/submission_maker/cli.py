188b8aa86acee0f5301690b8a3205e8fe3ff8e39
krzysiek <krzysztof.bozek@cern.ch>
2021-06-14 12:56:11 +0200

    Adding schema validation option

--- src/submission_maker/cli.py
+++ src/submission_maker/cli.py
@@ -1,6 +1,6 @@
 import click
 from .Submission import Submission
-
+from . import utils 
 
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
 def submission_maker():
@@ -8,15 +8,21 @@ def submission_maker():
 
 
 @click.command()
-@click.argument('steering_script',type=click.Path(exists=True))
-def create_submission(steering_script):
-    print(f"Creating submission file based on {steering_script}:")
+@click.argument('steering_file',type=click.Path(exists=True))
+def create_submission(steering_file):
+    print(f"Creating submission file based on {steering_file}:")
     submission=Submission()
-    submission.load_table_config(steering_script)
+    submission.load_table_config(steering_file)
     submission.implement_table_config()
     submission.create_hepdata_record()
     print("Submission created in test_submission")
 
-
+@click.command()
+@click.argument('steering_file',type=click.Path(exists=True))
+def check_schema(steering_file):
+    print(f"Checking schema of {steering_file}:")
+    utils.check_schema(steering_file,'steering_file.json')
+    print("All ok!")
 submission_maker.add_command(create_submission)
+submission_maker.add_command(check_schema)
 
