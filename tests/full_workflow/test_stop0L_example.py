0561f5a957177e1b555168e29a439c241e0938b4
krzysiek <krzysztof.bozek@cern.ch>
2021-08-10 08:48:40 +0200

    Fixing full_workflow test (Adding overview.yaml to list of expected yamls)

--- tests/full_workflow/test_stop0L_example.py
+++ tests/full_workflow/test_stop0L_example.py
@@ -21,7 +21,8 @@ expected_yamls=['acc_srat0.yaml',
                 'eff_sratw.yaml',
                 'eff_srbtw.yaml',
                 'eff_srbt0.yaml',
-                'cutflow_sratt.yaml']
+                'cutflow_sratt.yaml',
+                'overview.yaml']
 expected_pngs=['figaux_03b.png',
               'thumb_fig_12a.png',
               'thumb_tabaux_01.png',
@@ -50,12 +51,25 @@ basenames=[os.path.basename(x) for x in names]
 yaml_files=[x for x in basenames if re.match(".*.yaml$",x)]
 png_files=[x for x in basenames if re.match(".*.png$",x)]
 
+def diff(current,expected):
+    set1=set(expected)-set(current)
+    set2=set(current)-set(expected)
+    return set1.union(set2)
+
+def print_file_diff(current,expected):
+    print(f"current: {current}")
+    print(f"expected: {expected}")
+    print(f"diff: {diff(current,expected)}")
 
 if(len(yaml_files)!=len(expected_yamls)):
-    raise ValueError("Different number of yaml files expected!")
+    print_file_diff(yaml_files,expected_yamls)
+    raise ValueError(f"Different number of yaml files expected! Expected {len(expected_yamls)}, is: {len(yaml_files)}")
 if(len(png_files)!=len(expected_pngs)):
-    raise ValueError("Different number of yaml files expected!")
+    print_file_diff(png_files,expected_pngs)
+    raise ValueError(f"Different number of png files expected! Expected {len(expected_pngs)}, is: {len(png_files)}")
 if(set(yaml_files)!=set(expected_yamls)):
+    print_file_diff(yaml_files,expected_yamls)
     raise ValueError("Different names expected for yaml files!")
 if(set(png_files)!=set(expected_pngs)):
-    raise ValueError("Different names expected for yaml files!")
+    print_file_diff(png_files,expected_pngs)
+    raise ValueError("Different names expected for png files!")
