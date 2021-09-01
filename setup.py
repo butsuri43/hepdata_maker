3967316f7a75d959398ecd7f47fa059c820ec009
krzysiek <krzysztof.bozek@cern.ch>
2021-09-01 10:40:27 +0200

    Adding missing sphinx links for automatic documentation updates

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-setup()
+extras_require={}
+extras_require['docs'] = sorted(
+    set(
+        [
+            'sphinx-click',
+            'sphinx-copybutton',
+            'autoclasstoc',
+        ]
+    )
+)
+setup(extras_require=extras_require)
